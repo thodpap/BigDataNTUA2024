10a58cba94ed3d0f0996816ef1652e8b57d12de9
Thodoris Paparrigopoulos <paparrigopoulosthodoris@gmail.com>
2024-05-26 11:46:50 +0300

    parquet writing + parquet_sql

--- main.py
+++ main.py
@@ -2,16 +2,13 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#!pip install pyspark
 
 from pyspark.sql import SparkSession, Window
 from pyspark.sql.functions import col, rank
 
 
-def Q1_df():
-    spark = SparkSession.builder.appName("Top3MonthsCrimes").getOrCreate()
-
-    df = spark.read.csv("data/Crime_Data_from_2010_to_2019.csv", header=True, inferSchema=True)
-
+def sql_(df):
     # TODO: Ask about DATE OCC or DATE RPRT
     df = df.withColumn("year", col("DATE OCC").substr(7, 4))
     df = df.withColumn("month", col("DATE OCC").substr(0, 2))
@@ -28,24 +25,47 @@ def Q1_df():
     the person that came in third place (after the ties) would register as coming in fifth.'''
     # TODO: Ask about rank or dense rank
     df = df.withColumn("rank", rank().over(window_spec))
-
     df = df.orderBy(col("year"), col("crime_total").desc())
     df = df.where(col("rank") <= 3)
 
     df.show()
 
-    return df
 
-def Q1_parquet_sql():
-    spark = SparkSession.builder.appName("Top3MonthsCrimes").getOrCreate()
+class Q1:
+    def __init__(self, name, csv_path="data/Crime_Data_from_2010_to_2019.csv", parquet_path="data/Crime_Data_from_2010_to_2019.parquet"):
+        self.spark = SparkSession.builder.appName(name).getOrCreate()
+        self.csv_path = csv_path
+        self.parquet_path = parquet_path
+        try:
+            self.write_parquet()
+        except Exception as e:
+            print(e)
 
-    df = spark.read.csv("data/Crime_Data_from_2010_to_2019.csv", header=True, inferSchema=True)
+    def write_parquet(self):
+        # Read the CSV file into a DataFrame
+        df = self.spark.read.csv(self.csv_path, header=True, inferSchema=True)
+
+        # Write the DataFrame to Parquet format
+        df.write.parquet(self.parquet_path)
+
+    def csv_sql(self):
+        df = self.spark.read.csv("data/Crime_Data_from_2010_to_2019.csv", header=True, inferSchema=True)
+        sql_(df)
+
+    def parquet_sql(self):
+        df = self.spark.read.parquet("data/Crime_Data_from_2010_to_2019.parquet", header=True, inferSchema=True)
+        sql_(df)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    Q1_df()
+    # convert_csv_to_parquet("data/Crime_Data_from_2010_to_2019.csv", "data/Crime_Data_from_2010_to_2019.parquet")
+    Q1 = Q1("Top3MonthsCrimes", "data/Crime_Data_from_2010_to_2019.csv", "data/Crime_Data_from_2010_to_2019.parquet")
+    print("Parquet")
+    Q1.parquet_sql()
 
+    print("CSV")
+    Q1.csv_sql()
 
     # Filter
     # df.select("year").where(col("year") == "2010").show(15)
